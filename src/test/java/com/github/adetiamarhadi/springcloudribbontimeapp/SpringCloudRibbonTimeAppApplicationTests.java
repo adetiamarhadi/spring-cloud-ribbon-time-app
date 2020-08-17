a0ae2bb83011576fc57ca5f00f2caf6297313dde
adetiamarhadi <adetia.marhadi@gmail.com>
2020-08-17 09:44:27 +0700

    demo spring cloud ribbon

---
+++ src/test/java/com/github/adetiamarhadi/springcloudribbontimeapp/SpringCloudRibbonTimeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adetiamarhadi.springcloudribbontimeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudRibbonTimeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
